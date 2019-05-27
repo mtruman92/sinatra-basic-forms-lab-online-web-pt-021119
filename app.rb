773fd994a85d5a320ba94d58ace19f25b53ceea0
Mickae Truman <mtruman@nyit.edu>
2019-05-27 22:24:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,14 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
-  get '/' do
-    erb :homepage
-  end
-  
+
   get '/' do
     erb :index
   end
+  
+  get "/new" do
+      erb :create_puppy
+    end
 
 post "/puppy" do
       @puppy = Puppy.new(params)
