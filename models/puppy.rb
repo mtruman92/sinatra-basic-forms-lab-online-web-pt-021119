e67131b3d0023a9d18de8295657b71ed0dbe6341
Mickae Truman <mtruman@nyit.edu>
2019-05-27 22:19:43 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,7 +2,7 @@ class Puppy
   
   attr_accessor :name, :breed, :months_old
   
-  def initialize(puppy)
+  def initialize(params)
     @name = name
     @breed = breed 
     @months_old = months_old 
