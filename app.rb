5617e3fb6d7d2157a6ec0303002e89f2f072c7f5
Mickae Truman <mtruman@nyit.edu>
2019-05-27 22:27:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
       erb :create_puppy
     end
 
-post "/puppy" do
+  post "/puppy" do
       @puppy = Puppy.new(params)
       @name = @puppy.name
       @breed = @puppy.breed
